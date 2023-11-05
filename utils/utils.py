4b1cca9786b561694bb9cc327d499f00ebed3e77
Ata-Pab <52567674+Ata-Pab@users.noreply.github.com>
2023-11-05 09:35:11 +0300

    patchify method update

--- utils/utils.py
+++ utils/utils.py
@@ -465,7 +465,20 @@ def patchify_images(img_file_list, patch_size=256, method='CROP', scl=False, cvt
     ! pip install patchify
     Modified code of Dr. Sreenivas Bhattiprolu
     '''
-    ! pip install patchify
+    package = 'patchify'
+
+    try:
+      return __import__(package)
+    except ImportError:
+      import sys
+      import subprocess
+      # implement pip as a subprocess:
+      subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+      # process output with an API in the subprocess module:
+      reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+      installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+      print(installed_packages)
+       
     from patchify import patchify
 
     image_dataset = []
