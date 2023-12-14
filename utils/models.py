6758f37e27ac55cf18e0d51a74d676e2590733bd
Ata-Pab <52567674+Ata-Pab@users.noreply.github.com>
2023-12-14 15:35:04 +0300

    attention_block from model.py was updated

--- utils/models.py
+++ utils/models.py
@@ -81,34 +81,35 @@ def gating_signal(input, out_size, batch_norm=False):
 
 # Sample attention block
 def attention_block(x, gating, inter_shape):
-  shape_x = K.int_shape(x)  # x
-  shape_g = K.int_shape(gating)  # g
+    shape_x = K.int_shape(x)  # x
+    shape_g = K.int_shape(gating)  # g
 
-  # Getting x to the same shape as gating signal (128 to 64)
-  theta_x = layers.Conv2D(inter_shape, (2,2), strides=(2,2), padding='same')(x)
-  shape_theta_x = K.int_shape(theta_x)
+    # Getting x to the same shape as gating signal (128 to 64)
+    theta_x = layers.Conv2D(inter_shape, (2,2), strides=(2,2), padding='same')(x)
+    shape_theta_x = K.int_shape(theta_x)
 
-  # Getting the gating signal to the same number of filters as the inter_shape
-  phi_g = layers.Conv2D(inter_shape, (1,1), padding='same')(gating)
-  upsample_g = layers.Conv2DTranspose(inter_shape, (3, 3),
-                      strides=(shape_theta_x[1] // shape_g[1], shape_theta_x[2] // shape_g[2]),
-                      padding='same')(phi_g)  # 16
+    # Getting the gating signal to the same number of filters as the inter_shape
+    phi_g = layers.Conv2D(inter_shape, (1,1), padding='same')(gating)
+    upsample_g = layers.Conv2DTranspose(inter_shape, (3, 3),
+                        strides=(shape_theta_x[1] // shape_g[1], shape_theta_x[2] // shape_g[2]),
+                        padding='same')(phi_g)  # 16
 
-  concat_xg = layers.add([upsample_g, theta_x])
-  act_xg = layers.Activation('relu')(concat_xg)
+    concat_xg = layers.add([upsample_g, theta_x])
+    act_xg = layers.Activation('relu')(concat_xg)
 
-  psi = layers.Conv2D(1, (1,1), padding='same')(act_xg)
-  sigmoid_xg = layers.Activation('sigmoid')(psi)
-  shape_sigmoid = K.int_shape(sigmoid_xg)
+    psi = layers.Conv2D(1, (1,1), padding='same')(act_xg)
+    sigmoid_xg = layers.Activation('sigmoid')(psi)
+    shape_sigmoid = K.int_shape(sigmoid_xg)
 
-  upsample_psi = layers.UpSampling2D(size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2]))(sigmoid_xg)
+    upsample_psi = layers.UpSampling2D(size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2]))(sigmoid_xg)
 
-  y = layers.multiply([upsample_psi, x])
+    upsample_psi = repeat_elem(upsample_psi, shape_x[3])
+    y = layers.multiply([upsample_psi, x])
 
-  result = layers.Conv2D(shape_x[3], (1,1), padding='same')(y)
-  result_bn = layers.BatchNormalization()(result)
+    result = layers.Conv2D(shape_x[3], (1,1), padding='same')(y)
+    result_bn = layers.BatchNormalization()(result)
 
-  return result_bn
+    return result_bn
 
 def UNet(input_shape, filter_size=3, filter_num=64, up_sample_size=2, num_classes=1, dropout_rate=0.0, batch_norm=True, verbose=0):
     '''
