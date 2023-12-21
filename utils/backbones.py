41e5f9470531752fda34e43ae118cc90b328c1e1
Ata-Pab <52567674+Ata-Pab@users.noreply.github.com>
2023-12-21 14:53:20 +0300

    build_ConvAEModelV1 method was updated

--- utils/backbones.py
+++ utils/backbones.py
@@ -1,9 +1,22 @@
 import tensorflow as tf
-from utils.attention_modules import Conv2DLayerBN, Conv2DLayerRes, CBAM, AttentionLayerUNet
+from attention_modules import Conv2DLayerBN, Conv2DLayerRes, CBAM, AttentionLayerUNet
+import warnings
 
 def build_ConvAEModelV1(input_shape, latent_dim, layer_sizes=[32, 64, 128, 128, 256, 256, 256], **kwargs):
     num_strides = len(layer_sizes)
+
+    if (input_shape[0] < 224) or (input_shape[1] < 224):
+        raise ValueError("Input width or height cannot be lower than 224")
+    
     num_dense = int(input_shape[0]/(2**num_strides))
+    use_dense_layers = True
+
+    if (num_dense*num_dense*layer_sizes[-1]) > 4096:
+        use_dense_layers = False
+        warnings.warn("The number of neurons in the Dense layer should not exceed 2048 to fit into RAM. Resize image (downsize) or increase number of layers to be applied! Dense layers will not be used in encoder part. It makes accuracy down.", ResourceWarning)
+        #raise ValueError("The number of neurons in the Dense layer should not exceed 2048 to fit into RAM. Resize image (downsize) or increase number of layers to be applied!")
+    
+    if num_dense <= 0: num_dense = 1
     _kernel_size=5
     _strides=2
     _act_end="lrelu"
@@ -41,22 +54,23 @@ def build_ConvAEModelV1(input_shape, latent_dim, layer_sizes=[32, 64, 128, 128, 
 
     # Flatten layer
     x = tf.keras.layers.Flatten(name='flatten_layer')(x)
-    x = tf.keras.layers.Dense(units=(num_dense*num_dense*256))(x)
+    if use_dense_layers:
+        x = tf.keras.layers.Dense(units=(num_dense*num_dense*layer_sizes[-1]))(x)
+        #x = BatchNormalization(name='bacth_norm_1')(x)
+        x = tf.keras.layers.LeakyReLU(alpha=_lrelu_alpha)(x)
 
-    #x = BatchNormalization(name='bacth_norm_1')(x)
-    x = tf.keras.layers.LeakyReLU(alpha=_lrelu_alpha)(x)
     # Latent vector
     x = tf.keras.layers.Dense(units=latent_dim, name='latent_layer')(x)
     x = tf.keras.layers.LeakyReLU(alpha=_lrelu_alpha)(x)
 
     # Decoder Part
-    x = tf.keras.layers.Dense(units=(num_dense*num_dense*256))(x)
+    x = tf.keras.layers.Dense(units=(num_dense*num_dense*layer_sizes[-1]))(x)
     #x = BatchNormalization(name='batch_norm_2')(x)
     x = tf.keras.layers.LeakyReLU(alpha=_lrelu_alpha)(x)
 
-    x = tf.keras.layers.Reshape((num_dense,num_dense,256), name='reshape_latent')(x)
+    x = tf.keras.layers.Reshape((num_dense,num_dense,layer_sizes[-1]), name='reshape_latent')(x)
 
-    # Transposed Conv2D block
+    # Transposed Conv2D blocks
     for ix, num_filter in enumerate(layer_sizes[-2::-1]):  # Reverse layer_sizes list and remove first element of the reversed array
         _name = 'conv2d_transpose_block_' + str((ix+1))
         x = tf.keras.layers.Conv2DTranspose(filters=num_filter, kernel_size=_kernel_size, 
